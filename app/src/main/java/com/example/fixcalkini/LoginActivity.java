9ef62cae592d0101bdd07bc36d55e4a7bc401d3d
Oscar Garma <spongetcrazy@gmail.com>
2025-03-14 12:49:01 -0600

    Fix login and add main

--- app/src/main/java/com/example/fixcalkini/LoginActivity.java
+++ app/src/main/java/com/example/fixcalkini/LoginActivity.java
@@ -3,18 +3,27 @@ package com.example.fixcalkini;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
-    Button btnLogin = findViewById(R.id.button_login);
+    private ImageView viewImg;
+    private Button btnLogin; // Declarar aquí, pero inicializar en onCreate
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        viewImg = findViewById(R.id.idcarrite);
+        btnLogin = findViewById(R.id.btn_inicio); // 🔹 Inicializar aquí
+
+        Animation fromTop = AnimationUtils.loadAnimation(this, R.anim.anim_from_top);
+        viewImg.startAnimation(fromTop);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,3 +35,4 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 }
+
---
+++ app/src/test/java/com/example/fixcalkini/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.example.fixcalkini;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ * <p>
+ * See <a href="http://d.android.com/tools/testing">testing documentation</a>.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
