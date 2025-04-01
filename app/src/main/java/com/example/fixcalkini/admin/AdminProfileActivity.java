285684bc8e89b03f432fa2b98f32fe4d0e01345b
gildardo13 <gildardo.david13@gmail.com>
2025-04-01 02:22:45 -0600

    agregar la lista de reportes en el administrador

--- app/src/main/java/com/example/fixcalkini/admin/AdminProfileActivity.java
+++ app/src/main/java/com/example/fixcalkini/admin/AdminProfileActivity.java
@@ -1,6 +1,10 @@
 package com.example.fixcalkini.admin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +13,70 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.fixcalkini.R;
+import com.example.fixcalkini.ToolBox;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AdminProfileActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    ImageButton btnBack;
+
+    TextView etNombre, txtEmail, txtReportes,txtRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_admin_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        btnBack = findViewById(R.id.btnBack);
+
+
+        etNombre = findViewById(R.id.etNombre);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtRegistro = findViewById(R.id.txtRegistro);
+        txtReportes = findViewById(R.id.txtReportes);
+
+        txtEmail.setText(ToolBox.obtenerCorreo(getApplicationContext()));
+
+        // Obtener el número de reportes
+        CollectionReference reportesRef = db.collection("reportes");
+        reportesRef.whereEqualTo("estado", "aceptado")
+                .addSnapshotListener((value, error) -> {
+                    if (error != null) {
+                        return;
+                    }
+
+                    if (value != null) {
+                        txtReportes.setText(String.valueOf(value.size())); // Directamente obtenemos la cantidad
+                    } else {
+                        txtReportes.setText("0"); // Si no hay datos, mostramos 0
+                    }
+                });
+        db.collection("users").document(txtEmail.getText().toString()).get().addOnSuccessListener(documentSnapshot -> {
+            if (documentSnapshot.exists()) {
+                String nombre = documentSnapshot.getString("nombre");
+                String fecha = documentSnapshot.getString("fecha_registro");
+
+                etNombre.setText(nombre);
+                txtRegistro.setText(fecha);
+            } else {
+                Toast.makeText(getApplicationContext(), "No se encontró el usuario en la base de datos", Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(e -> {
+            Toast.makeText(getApplicationContext(), "Error al obtener el nombre", Toast.LENGTH_SHORT).show();
+
         });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/fixcalkini/admin/AdminReportesAceptadosActivity.java
+++ app/src/main/java/com/example/fixcalkini/admin/AdminReportesAceptadosActivity.java
@@ -1,26 +1,122 @@
 package com.example.fixcalkini.admin;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fixcalkini.R;
+import com.example.fixcalkini.Reporte;
+import com.example.fixcalkini.ReporteAdapter;
+import com.example.fixcalkini.ToolBox;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminReportesAceptadosActivity extends AppCompatActivity {
-
+    ImageButton btnBack;
+    private RecyclerView recyclerView;
+    private ReporteAdapter adapter;
+    private List<Reporte> listaReportes = new ArrayList<>();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private View txtSinReportes; // Declarar como variable de clase
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_admin_reportes_aceptados);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Configurar RecyclerView
+        recyclerView = findViewById(R.id.recyclerReportes);
+        txtSinReportes = findViewById(R.id.txtSinReportes); // Guardar referencia
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ReporteAdapter(listaReportes, this::verDetalles);
+        recyclerView.setAdapter(adapter);
+
+        // Cargar los reportes desde Firebase
+        cargarReportes();
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
+    }
+    private void cargarReportes() {
+        String usuarioCorreo = ToolBox.obtenerCorreo(getApplicationContext()); // Obtener el correo del usuario autenticado
+        if (usuarioCorreo == null || usuarioCorreo.isEmpty()) {
+            Toast.makeText(this, "Error: No se encontró el usuario autenticado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        CollectionReference reportesRef = db.collection("reportes");
+
+        // Filtrar reportes donde el propietario sea el usuario autenticado
+        reportesRef.whereEqualTo("estado", "arreglado")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+                            Toast.makeText(getApplicationContext(), "Error al cargar reportes", Toast.LENGTH_SHORT).show();
+                            Log.e("FirebaseReportes", "Error al obtener reportes", error);
+                            return;
+                        }
+
+                        if (value == null) {
+                            Log.e("FirebaseReportes", "No se encontraron reportes o hubo un error.");
+                            return;
+                        }
+
+                        listaReportes.clear();
+                        for (QueryDocumentSnapshot document : value) {
+                            String id = document.getId(); // Obtener el ID del documento
+                            String titulo = document.getString("titulo");
+                            String descripcion = document.getString("propietario");
+                            Double latitud = document.getDouble("latitud");
+                            Double longitud = document.getDouble("longitud");
+                            String estado = document.getString("estado");
+                            String timestamp = document.getString("timestamp");
+                            // Imprimir en la consola de Logcat
+                            Log.d("FirebaseReportes", "ID: " + id + ", Título: " + titulo + ", Descripción: " + descripcion + " " + latitud + " " + longitud + " " + timestamp);
+
+                            // Agregar a la lista solo con título y descripción
+                            listaReportes.add(new Reporte(id, titulo, descripcion, latitud, longitud, estado, timestamp));
+                        }
+
+                        adapter.notifyDataSetChanged();
+
+                        // Mostrar RecyclerView si hay reportes, o el mensaje de "No hay reportes"
+
+                        if (listaReportes.isEmpty()) {
+                            recyclerView.setVisibility(View.GONE);
+                            txtSinReportes.setVisibility(View.VISIBLE);
+                        } else {
+                            recyclerView.setVisibility(View.VISIBLE);
+                            txtSinReportes.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    } // Método para mostrar la alerta con el ID del reporte
+
+    private void verDetalles(@NonNull Reporte reporte) {
+
     }
 }
